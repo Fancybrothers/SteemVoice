d40617e35515eaa8cdcd52ff20e9bd8c6eb80638
Fancybrothers <40843409+Fancybrothers@users.noreply.github.com>
2018-11-10 17:58:34 +0100

    Added 6 new commands
    I added 6 new commands which are :
    Getting the price of SBD and Steem.
    Getting a user's available Steem and SBD.
    Calculating a user's account worth.
    Getting a user's savings.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from steem.converter import Converter
 from steem.blog import Blog
 from steem.account import Account
 from steem.amount import Amount
+import requests
 
 St_username = ""
 s = Steem()
@@ -19,7 +20,10 @@ class Steemian:
         self.reputation = str(self.data.rep)
         self.upvoteworth = self.calculate_voteworth()
         self.steempower = self.calculate_steempower()
-		
+        self.wallet = self.data.balances
+        self.accountworth = self.calculate_accountworth()
+        self.steemprice = self.cmc_price('1230')
+        self.sbdprice = self.cmc_price('1312')
 		
     def calculate_voteworth(self):
         reward_fund = s.get_reward_fund()
@@ -43,6 +47,18 @@ class Steemian:
         delegated = Amount(self.data['delegated_vesting_shares']).amount
         received = Amount(self.data['received_vesting_shares']).amount
         return('Your total Steem Power is %s ...  You own %s. You are delegating %s and you are receiving %s.'% (vests2sp(total),vests2sp(owned),vests2sp(delegated), vests2sp(received)))
+	
+    def cmc_price(self,id):
+        r = requests.get("https://api.coinmarketcap.com/v2/ticker/"+id)
+        data = r.json()
+        return data['data']['quotes']['USD']['price']
+		
+    def calculate_accountworth(self):
+        SBD_price = self.cmc_price('1312')
+        STEEM_price = self.cmc_price('1230')
+        accountworth = (self.wallet['total']['STEEM'] + self.data.sp)*STEEM_price + self.wallet['total']['SBD']*SBD_price
+        return round(accountworth,0)
+        
 		
 # Setting a new username and changing it
 @assist.action('Change_username')
@@ -82,6 +98,48 @@ def r_last_post():
     post = b.take(1)
     return ask('Your latest post is: \n'+ post[0]['title'])
 
+# Is used to get the available steem of a given user
+
+@assist.action('steem')
+def r_steem():	
+    user = Steemian(St_username)
+    return ask('%s STEEM' % user.wallet['available']['STEEM'])
+	
+# Is used to get the available sbd of a given user
+
+@assist.action('sbd')
+def r_sbd():	
+    user = Steemian(St_username)
+    return ask('%s Steem Dollars' % user.wallet['available']['SBD'])
+	
+# Is used to get the savings of a given user
+
+@assist.action('savings')
+def r_savings():	
+    user = Steemian(St_username)
+    return ask('You have %s Steem Dollars and %s STEEM in your savings' % (user.wallet['savings']['SBD'],user.wallet['savings']['STEEM']))
+
+# Is used to calculate the account worth
+
+@assist.action('accountworth')
+def r_accountworth():	
+    user = Steemian(St_username)
+    return ask('Your account is worth approximately $%i according to coinmarketcap\'s latest prices.' % user.accountworth)
+	
+# Is used to get the price of SBD
+
+@assist.action('sbdprice')
+def r_sbdprice():	
+    user = Steemian(St_username)
+    return ask('Steem Dollars is now worth $'+str(round(user.sbdprice,2))+' according to coinmarketcap.')
+	
+# Is used to get the price of STEEM
+
+@assist.action('steemprice')
+def r_steemprice():	
+    user = Steemian(St_username)
+    return ask('Steem is now worth $'+str(round(user.steemprice,2))+' according to coinmarketcap.')
+						
 # run Flask app
 if __name__ == '__main__':
     app.run(debug=True)
